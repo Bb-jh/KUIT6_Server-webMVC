4777d7ba6f72c4d00d97f2b6b07613b13d22ceba
Bb-jh <august0511@naver.com>
2025-10-08 14:45:57 +0900

    feat: 로그인 Controller 생성

---
+++ src/main/java/jwp/controller/LoginUserController.java
@@ -0,0 +1,32 @@
+package jwp.controller;
+
+import core.db.MemoryUserRepository;
+import jwp.model.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/user/login")
+public class LoginUserController extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        User user = MemoryUserRepository.getInstance().findUserById(req.getParameter("userId"));
+
+        if (user == null || !user.matchPassword(req.getParameter("password"))) {
+            System.out.println("user 로그인 실패");
+            resp.sendRedirect("/user/loginFailed.jsp");
+            return;
+        }
+
+        HttpSession session = req.getSession();
+        session.setAttribute("user", user);
+
+        resp.sendRedirect("/");
+//        super.doPost(req, resp);
+    }
+}
