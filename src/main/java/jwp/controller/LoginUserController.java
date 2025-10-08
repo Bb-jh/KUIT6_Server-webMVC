53e6656e16b3685066c46d2ddd7d87975cd4b134
Bb-jh <august0511@naver.com>
2025-10-08 16:41:56 +0900

    feat: 로그인 했을 때만 list 페이지 접근 허용

--- src/main/java/jwp/controller/LoginUserController.java
+++ src/main/java/jwp/controller/LoginUserController.java
@@ -3,6 +3,7 @@ package jwp.controller;
 import core.db.MemoryUserRepository;
 import jwp.model.User;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,6 +14,12 @@ import java.io.IOException;
 
 @WebServlet("/user/login")
 public class LoginUserController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher rd = req.getRequestDispatcher("/user/login.jsp");
+        rd.forward(req, resp);
+    }
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         User user = MemoryUserRepository.getInstance().findUserById(req.getParameter("userId"));
